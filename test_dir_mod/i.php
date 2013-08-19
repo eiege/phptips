a84b134ddc092b39c2db3f59833b6dad1b19c2da
gaocheng <gaoc@kingnet.com>
2013-08-19 10:58:40 +0800

    增加新的tips的模板
    增加新tips的文件和结构模板

---
+++ test_dir_mod/i.php
@@ -0,0 +1,30 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <script type="text/javascript" src="../base_file/jq1.7min.js" />
+    <title></title>
+</head>
+<body>
+
+<?php
+
+
+?>
+
+<?php
+function pm($t, $c)
+{
+    echo "<pre>";
+    echo "<h4>" . $t . "</h4><hr />";
+    var_dump($c);
+    echo "</pre>";
+}
+
+?>
+</body>
+</html>
