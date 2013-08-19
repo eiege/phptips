63fcc6b49a558e34c03b0e640d2793f79c0b61b6
gaocheng <gaoc@kingnet.com>
2013-08-19 10:54:34 +0800

    添加文件
    添加文件

---
+++ areq_check/after.php
@@ -0,0 +1,12 @@
+<pre>
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: gaocheng
+ * Date: 13-8-12
+ * Time: 下午3:38
+ * To change this template use File | Settings | File Templates.
+ */
+var_dump($_POST);
+?>
+</pre>
